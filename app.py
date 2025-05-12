620647308d39ebcf419dfdce8531fcfde2fbe4f4
Kareem <117325791+kareem1099@users.noreply.github.com>
2025-05-12 15:47:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -32,3 +32,6 @@ async def predict_lung(file: UploadFile = File(...)):
         return {"result": predicted_class}
     except Exception as e:
         return JSONResponse({"error": str(e)})
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
